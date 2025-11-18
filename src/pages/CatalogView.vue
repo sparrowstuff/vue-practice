aa1af3f4dd9ac68e644484daa197352b58cdfe4d
sparrowstuff <damnagett@gmail.com>
2025-11-18 13:38:35 +0300

    добавлена фильтрация по цене

--- src/pages/CatalogView.vue
+++ src/pages/CatalogView.vue
@@ -1,22 +1,48 @@
 <template>
-  <h2 class="Catalog" style="margin-bottom: 1.12rem">Catalog</h2>
-  <div class="catalog-inputs">
-    <div class="custom-input">
-      <label for="sort" class="custom-input__label">Что вас интересует?</label>
-      <input
-        type="text"
-        id="sort"
-        class="custom-input__input"
-        placeholder="Поиск"
-        v-model="searchText"
-      />
-    </div>
-  </div>
   <div class="catalog">
-    <ProductCard v-for="product in filteredProducts" :key="product.id" :product="product" />
-  </div>
-  <div v-if="filteredProducts.length === 0 && products.length > 0">
-    Ничего не найдено по запросу {{ searchText }}
+    <h2 class="catalog__title" style="margin-bottom: 1.12rem">Catalog</h2>
+    <div class="catalog__inputs">
+      <div class="custom-input">
+        <label for="searchTitle" class="custom-input__label">Название: </label>
+        <input
+          type="text"
+          id="searchTitle"
+          class="custom-input__input"
+          placeholder="Поиск"
+          v-model="searchText"
+        />
+      </div>
+      <div class="custom-input">
+        <label for="searchMin" class="custom-input__label">Цена&nbsp;от: </label>
+        <input
+          type="number"
+          min="1"
+          max="100"
+          id="searchMin"
+          class="custom-input__input"
+          placeholder="$"
+          v-model="searchMin"
+        />
+      </div>
+      <div class="custom-input">
+        <label for="searchMax" class="custom-input__label">Цена&nbsp;до: </label>
+        <input
+          type="number"
+          min="1"
+          max="100"
+          id="searchMax"
+          class="custom-input__input"
+          placeholder="$"
+          v-model="searchMax"
+        />
+      </div>
+    </div>
+    <div class="catalog__cards-block">
+      <ProductCard v-for="product in filteredProducts" :key="product.id" :product="product" />
+    </div>
+    <div v-if="filteredProducts.length === 0 && products.length > 0">
+      Ничего не найдено по запросу {{ searchText }}
+    </div>
   </div>
 </template>
 
@@ -27,6 +53,8 @@ import { ref, watch, computed, onMounted } from 'vue'
 
 const products = ref([])
 const searchText = ref('')
+const searchMin = ref('')
+const searchMax = ref('')
 
 const loadData = async () => {
   const data = await getProducts()
@@ -38,13 +66,23 @@ onMounted(() => {
 })
 
 const filteredProducts = computed(() => {
-  if (!searchText.value.trim()) {
-    return products.value
+  let filtered = products.value
+
+  if (searchText.value) {
+    filtered = filtered.filter((product) =>
+      product.title.toLowerCase().includes(searchText.value.toLowerCase()),
+    )
   }
 
-  // ✅ Исправленная фильтрация
-  const searchLower = searchText.value.toLowerCase()
-  return products.value.filter((product) => product.title.toLowerCase().includes(searchLower))
+  if (searchMin.value) {
+    filtered = filtered.filter((product) => product.price >= Number(searchMin.value))
+  }
+
+  if (searchMax.value) {
+    filtered = filtered.filter((product) => product.price <= Number(searchMax.value))
+  }
+
+  return filtered
 })
 </script>
 
@@ -54,13 +92,60 @@ h2 {
   line-height: 100%;
 }
 
-.catalog {
+.catalog__inputs {
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  gap: 0.62rem;
+  width: 100%;
+  margin-bottom: 1.12rem;
+}
+
+.catalog__cards-block {
   display: flex;
   flex-wrap: wrap;
   gap: 3vw;
 }
+.custom-input {
+  max-width: 200px;
+  width: 100%;
 
-.catalog-inputs {
-  margin-bottom: 1.12rem;
+  position: relative;
+}
+
+.custom-input__label {
+  position: absolute;
+  top: -34%;
+  left: 3%;
+  font-size: 1rem;
+  padding: 0.2rem 0.4rem 0.2rem 0.2rem;
+  border-radius: 5%;
+  background-color: white;
+  display: block;
+  color: aqua;
+
+  transition:
+    transform 0.3s ease-in,
+    scale 0.3s ease-in,
+    color 0.3s ease-in;
+}
+
+.custom-input:hover .custom-input__label,
+.custom-input:focus-within .custom-input__label,
+.custom-input:active .custom-input__label {
+  scale: 0.9;
+  color: black;
+}
+
+.custom-input__input {
+  width: 100%;
+  height: 2.5rem;
+  padding: 0.4rem 0.3rem 0.1rem 0.3rem;
+  border: unset;
+  border-radius: 0.5rem;
+  outline: 2px solid aqua;
+}
+.custom-input__input::placeholder {
+  font-size: 1.1rem;
 }
 </style>
